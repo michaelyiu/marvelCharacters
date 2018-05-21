c635e96a673a592a7eb4cb43f5cdd07dbdebbc3d
Michael Yiu <onew1ng3d@gmail.com>
2018-05-21 16:46:19 -0400

    changed directories for some funky stuff on ghpages 5, moved image subdirectories to root

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -60,8 +60,8 @@ class App extends React.Component {
             
             let characterObject = {
               name: character,
-              icon: `./thumbnails/${character}.png`,
-              banner: `./large-image/${character}.png`,
+              icon: `../thumbnails/${character}.png`,
+              banner: `../large-image/${character}.png`,
               iconClassName: 'character-icon',
               portraitClassName: 'character-portrait',
               fullName: affiliation[key][character]
